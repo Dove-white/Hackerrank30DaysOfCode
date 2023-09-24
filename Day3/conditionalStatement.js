6a3cb6555ae6713c34865b2af2a8489f51960a86
Dove-White <dugbazahredeemer@gmail.com>
2023-09-24 18:44:03 +0000

    inline coment correction

--- Day3/conditionalStatement.js
+++ Day3/conditionalStatement.js
@@ -1,11 +1,11 @@
 function ifOldOrEven(N) {
   let even = N % 2 === 0;
 
-  if (even && N <= 5) console.log("Not Weird");
-  else if (even && N <= 20) console.log("Weird");
-  else if (even && N > 20) console.log("Not Weird");
-  else console.log("Weird");
+  if(!even) console.log('Weird')
+  else if(even && N <= 5) console.log("Not Weird");
+  else if(even && N <= 20) console.log("Weird"); 
+  else console.log('Not Weird');
 }
 
 ifOldOrEven(3);
-ifOldOrEven(24);
+ifOldOrEven(25);
