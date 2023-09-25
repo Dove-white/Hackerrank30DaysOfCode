d6ef5f8ded838fe1487be19dd1d346c13a62d4f5
Dove-White <dugbazahredeemer@gmail.com>
2023-09-25 00:13:53 +0000

    ternary conditional statement

--- Day3/conditionalStatement.js
+++ Day3/conditionalStatement.js
@@ -1,11 +1,14 @@
 function ifOldOrEven(N) {
   let even = N % 2 === 0;
 
-  if(!even) console.log('Weird')
-  else if(even && N <= 5) console.log("Not Weird");
-  else if(even && N <= 20) console.log("Weird"); 
-  else console.log('Not Weird');
+  even && N <= 5
+    ? console.log("Not Weird")
+    : even && N <= 20
+    ? console.log("Weird")
+    : even && N > 20
+    ? console.log("Not Weird")
+    : console.log("Weird");
 }
 
 ifOldOrEven(3);
-ifOldOrEven(25);
+ifOldOrEven(24);
